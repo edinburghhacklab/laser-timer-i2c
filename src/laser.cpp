25d1ef8d5394966ccb10b1e23553a5b9e605a4e1
Simon Arlott <sa.me.uk>
2025-05-08 19:24:04 +0100

    Move laser timing to a separate class

---
+++ src/laser.cpp
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2025  Simon Arlott
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "laser.h"
+
+#include <Arduino.h>
+#include <hardware/gpio.h>
+#include <hardware/sync.h>
+
+static LaserTime *instance = nullptr;
+
+LaserTime::LaserTime(unsigned int gpio, bool active_low)
+		: gpio_(gpio), active_low_(active_low) {
+}
+
+void __not_in_flash_func(c_gpio_irq_handler)(unsigned int gpio, uint32_t event_mask) {
+	instance->gpio_irq_handler(gpio, event_mask);
+}
+
+void LaserTime::start() {
+	lock_ = spin_lock_init(spin_lock_claim_unused(true));
+
+	_gpio_init(LED_BUILTIN);
+	gpio_set_dir(LED_BUILTIN, GPIO_OUT);
+	gpio_put(LED_BUILTIN, false);
+
+	_gpio_init(gpio_);
+	gpio_set_dir(gpio_, GPIO_IN);
+	gpio_set_pulls(gpio_, active_low_, !active_low_);
+
+	instance = this;
+	gpio_set_irq_enabled_with_callback(gpio_,
+		GPIO_IRQ_LEVEL_LOW | GPIO_IRQ_LEVEL_HIGH, true,
+		&c_gpio_irq_handler);
+}
+
+uint64_t __not_in_flash_func(LaserTime::uptime_us)() {
+	return to_us_since_boot(get_absolute_time());
+}
+
+uint64_t __not_in_flash_func(LaserTime::laser_time_us)() const {
+	auto saved_irq = spin_lock_blocking(lock_);
+	uint64_t time_us = time_count_us_;
+
+	if (on_) {
+		time_us += uptime_us() - start_us_;
+	}
+
+	spin_unlock(lock_, saved_irq);
+	return time_us;
+}
+
+void __not_in_flash_func(LaserTime::gpio_irq_handler)(unsigned int gpio, uint32_t event_mask) {
+	if (gpio == gpio_) {
+		auto saved_irq = spin_lock_blocking(lock_);
+		uint64_t now_us = uptime_us();
+		bool raw_value = gpio_get(gpio);
+		bool value = raw_value ^ active_low_;
+
+		if (on_ != value) {
+			if (on_) {
+				time_count_us_ += now_us - start_us_;
+			}
+
+			start_us_ = now_us;
+			on_ = value;
+			gpio_change_count_++;
+			gpio_put(LED_BUILTIN, on_);
+		}
+
+		gpio_irq_count_++;
+		spin_unlock(lock_, saved_irq);
+
+		gpio_set_irq_enabled(gpio,
+			raw_value ? GPIO_IRQ_LEVEL_HIGH : GPIO_IRQ_LEVEL_LOW, false);
+		gpio_set_irq_enabled(gpio,
+			raw_value ? GPIO_IRQ_LEVEL_LOW : GPIO_IRQ_LEVEL_HIGH, true);
+	} else {
+		gpio_set_irq_enabled(gpio, event_mask, false);
+	}
+}
+
+size_t LaserTime::printTo(Print &p) const {
+	size_t len = 0;
+
+	len += p.print("{state: ");
+	len += p.print(on_);
+	len += p.print(", changes: ");
+	len += p.print(gpio_change_count_);
+	len += p.print(", interrupts: ");
+	len += p.print(gpio_irq_count_);
+	len += p.print(", time_us: ");
+	len += p.print(laser_time_us());
+	len += p.print("}");
+
+	return len;
+}
---
+++ src/laser.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2025  Simon Arlott
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <Arduino.h>
+#include <hardware/sync.h>
+
+class LaserTime: public Printable {
+public:
+	LaserTime(unsigned int gpio, bool active_low);
+
+	static uint64_t __not_in_flash_func(uptime_us)();
+
+	void start();
+	uint64_t __not_in_flash_func(laser_time_us)() const;
+	size_t printTo(Print &p) const override;
+
+private:
+	friend void __not_in_flash_func(c_gpio_irq_handler)(unsigned int gpio, uint32_t event_mask);
+	void __not_in_flash_func(gpio_irq_handler)(unsigned int gpio, uint32_t event_mask);
+
+	const unsigned int gpio_;
+	const bool active_low_;
+
+	bool on_ = false;
+	uint64_t start_us_ = 0;
+	uint64_t time_count_us_ = 0;
+	uint64_t gpio_change_count_ = 0;
+	uint64_t gpio_irq_count_ = 0;
+	spin_lock_t *lock_ = nullptr;
+};
